18c8702585a9f5d02a21dd6becca5094ae5e29f7
DjalmaFelipe <DjalmaFelipe@outlook.com>
2024-09-13 02:48:17 -0300

    inicializacao do projeto - spring web e spring data

---
+++ src/test/java/com/Springdata/datatest/DatatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Springdata.datatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
